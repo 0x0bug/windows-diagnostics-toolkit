90fffd9b161750dc1f8195fea426ff013a797297
0x0Bug <98243573+0x0bug@users.noreply.github.com>
2026-09-18 07:39:26 +0200

    fix: suppress serialized PowerShell progress output

--- scripts/process-runner.ps1
+++ scripts/process-runner.ps1
@@ -424,7 +424,7 @@ function Invoke-DiagnosticScript {
                 if ($argument -match '^-[A-Za-z][A-Za-z0-9]*$') { $argument }
                 else { "'" + $argument.Replace("'", "''") + "'" }
             })
-        $commandText = "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8; & '$escapedScriptPath' $($escapedArguments -join ' ')"
+        $commandText = "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8; `$ProgressPreference = 'SilentlyContinue'; & '$escapedScriptPath' $($escapedArguments -join ' ')"
         $encodedCommand = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commandText))
 
         $startInfo = New-Object System.Diagnostics.ProcessStartInfo
